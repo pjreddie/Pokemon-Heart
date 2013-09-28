1862db48de224462c76ce50446187bbffa87d53b
David Bell <dbell@middlebury.edu>
2013-09-28 13:48:27 -0700

    more stuff

--- app/scripts/monstermodels.js
+++ app/scripts/monstermodels.js
@@ -16,6 +16,7 @@ $.getJSON( "/pokemon.json", function(data) {
    $.each(data, function(key, val) {
       pokemon[key] = {
          "name"         : val.name,
+         "id_number"    : key,
          "hp"           : val.baseStats.hp,
          "atk"          : val.baseStats.atk,
          "def"          : val.baseStats.def,
@@ -26,41 +27,77 @@ $.getJSON( "/pokemon.json", function(data) {
    });
 });
 
-//var Attack = function (name, ap) {
-   //return {
-      //"name"         : name,
-      //"atk"  : atk
+var Monster = function(spec) {
+   var that = {};
+
+   that.name = spec.name;
+   that.maxHP = spec.hp;
+   that.currHP = spec.hp;
+   that.atk = spec.atk;
+   that.def = spec.def;
+   that.speed = spec.speed;
+
+
+   that.damage = function(dmg) {
+      that.currHP = that.currHP - dmg;
+      if (that.currHP < 0) {
+         console.log("Underflowed HP!");
+         that.currHP = 0;
+      }
+   };
+   
+   that.heal = function(hlth) {
+      that.currHP = that.currHP - hlth;
+      if (that.currHP > that.maxHP) { 
+         console.log("Overflowed HP!");
+         that.currHP = that.maxHP;
+      }
+   };
+
+   that.isDead = function() {
+      return that.hp < 0;
+   };
+
+   return that;
+}
+
+//var Attack = function(name) {
+   //for (m in moves)
+   //{
+      //var move = moves[m];
+
+      //console.log(move.name);
+
+      //if (move.name === name)
+      //{
+         //return {
+            //"name"      : move.name,
+            //"type"      : move.type,
+            //"maxPP"     : move.pp,
+            //"currentPP" : move.pp,
+            //"power"     : move.power
+         //}
+      //}
    //}
+
+   //return Attack("Struggle");
 //}
 
-var Monster = function (name) {
-   for (m in pokemon)
-   {
-      var monster = pokemon[m];
 
-      if (monster.name === name)
-      {
-         return {
-            "name"         : monster.name,
-            "maxHP"        : monster.hp,
-            "currentHP"    : monster.hp,
-            "atk"          : monster.atk,
-            "def"          : monster.def,
-            "speed"        : monster.speed,
-            "evolveTo"     : monster.evolveTo,
-            "evolveLevel"  : monster.evolveLevel
-         };
-      }
+var getMonster = function (id_number) {
+   if (id_number < 1 || id_number > 151)
+   {
+      return Monster({
+         "name"         : "MissingNo",
+         "maxHP"        : 9999,
+         "currentHP"    : 9999,
+         "atk"          : 9999,
+         "def"          : 9999,
+         "speed"        : 9999,
+         "evolveTo"     : -1,
+         "evolveLevel"  : -1
+      });
+   } else {
+      return Monster(pokemon[id_number]);
    }
-   // no monster data
-   return {
-      "name"         : "MissingNo",
-      "maxHP"        : 9999,
-      "currentHP"    : 9999,
-      "atk"          : 9999,
-      "def"          : 9999,
-      "speed"        : 9999,
-      "evolveTo"     : -1,
-      "evolveLevel"  : -1,
-   };
 }
