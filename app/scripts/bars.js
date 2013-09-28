2e494feda78874e5f53bfbee537b1ca584a60b46
Alex Bullard <alex@Funnerator.com>
2013-09-28 14:14:26 -0700

    bar

--- app/scripts/bars.js
+++ app/scripts/bars.js
@@ -2,9 +2,24 @@
  * @jsx React.DOM
  */
 
+var HealthBar = React.createClass({
+  getInitialState: function() {
+  },
+  handleChange: function(event) {
+  },
+  render: function() {
+    return (
+      <div class="progress progress-striped">
+        <div class="progress-bar progress-bar-danger" role="progressbar" style={{width: '80%'}}>
+          <span class="sr-only">80% Complete (danger)</span>
+        </div>
+      </div>
+    )
+  }
+})
+
 var TopBar = React.createClass({
   getInitialState: function() {
-    console.log('here')
   },
   handleChange: function(event) {
   },
@@ -14,8 +29,8 @@ var TopBar = React.createClass({
     }
     return (
       <div>
-        <div class='menuContainer'>
-          { menuItems.map(createItem) }
+        <div class='topBar'>
+          < HealthBar />
         </div>
       </div>
     );
