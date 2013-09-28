b5a29d624ec86a61a22b109787876968bed6bb7b
Alex Bullard <alex@Funnerator.com>
2013-09-28 11:36:47 -0700

    yeoman

---
+++ app/scripts/react.js
@@ -0,0 +1,10 @@
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>{'Hello ' + this.props.name}</div>;
+  }
+});
+
+React.renderComponent(
+  <HelloMessage name="John" />,
+  document.getElementById('container')
+);
